41d10e402fdd36176f6c35be190d290afb12e68d
Duy Phan <hoangduy28101997@gmail.com>
2024-01-10 22:28:35 +0700

    lab01 #2

--- ec2.tf
+++ ec2.tf
@@ -1,6 +1,6 @@
-resource "aws_instance" "ec2-lab" {
-  instance_type = "t2.micro"
-  subnet_id     = module.vpc.private_subnets[0]
-  ami = "ami-087c17d1fe0178315"
-  associate_public_ip_address = true
-}
+# resource "aws_instance" "ec2-lab" {
+#   instance_type = "t2.micro"
+#   subnet_id     = module.vpc.private_subnets[0]
+#   ami = "ami-087c17d1fe0178315"
+#   associate_public_ip_address = true
+# }
